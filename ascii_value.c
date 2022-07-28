63a02a10e9d518ebd7b5f07d37a65eb4543ae5c3
nehatkr <nehathakur2308@gmail.com>
2022-07-28 15:37:36 +0530

    Basic C Program

---
+++ ascii_value.c
@@ -0,0 +1,11 @@
+#include<stdio.h> //printing ascii value. (Ex - A = 65, a = 97 ,0 = 48 ,space = 32.)
+
+int main(){
+    char ch;
+    printf("Enter any character :");
+    scanf("%c" ,&ch);
+
+    printf("%d",ch);
+
+    return 0;
+}
