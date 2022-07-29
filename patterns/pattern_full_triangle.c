efb4220e06186c643a657a18d695768a9a7b0d3d
nehatkr <nehathakur2308@gmail.com>
2022-07-29 19:05:59 +0530

    day3

---
+++ patterns/pattern_full_triangle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int rows, space,n = 0;
+    printf("Enter the number of rows: ");
+    scanf("%d",&rows);
+
+   for (int i = 1; i <= rows; i++ ,n=0)
+    {
+        for (space = 1; space <=rows - i; ++space)
+        {
+            printf("  ");
+        }
+       while (n != 2 * i - 1) {
+        printf("* ");
+        ++n;
+       }
+       printf("\n");
+    }
+    
+return 0;
+}
