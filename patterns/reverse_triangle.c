da5952cdb2b21824d49e982b4a4eb3a3bc0e4899
nehatkr <nehathakur2308@gmail.com>
2022-07-29 17:02:48 +0530

    day3

--- patterns/reverse_triangle.c
+++ patterns/reverse_triangle.c
@@ -2,12 +2,12 @@
 
 int main(){
     int n;
-    printf("Enter the number");
+    printf("Enter the number: ");
     scanf("%d",&n);
 
-    for (int i = n; i > 1; i--)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = i; j <i+1; j++)
+        for (int j = n; j >=i; j--)
         {
             printf("* ");
         }
