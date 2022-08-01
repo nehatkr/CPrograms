798995e564386fd1725da861c52b197c4be3e806
nehatkr <nehathakur2308@gmail.com>
2022-08-01 18:11:28 +0530

    day5

---
+++ basic/switch_case.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+int main(){
+    int month;
+
+    printf("Enter number between 1-12: \n");
+    scanf("%d" , &month);
+
+    switch (month)
+    {
+    case 1:
+        printf("31 Days");
+        break;
+    case 2:
+        printf("28/29 Days");
+        break;
+    case 3:
+        printf("30 Days");
+        break;
+    case 4:
+        printf("31 Days");
+        break;
+    case 5:
+        printf("30 Days");
+        break;
+    case 6:
+        printf("31 Days");
+        break;
+    case 7:
+        printf("31 Days");
+        break;
+    case 8:
+        printf("31 Days");
+        break;
+    case 9:
+        printf("30 Days");
+        break;
+    case 10:
+        printf("31 Days");
+        break;
+    case 11:
+        printf("30 Days");
+        break;
+    case 12:
+        printf("31 Days");
+        break;
+
+    default:
+    printf("Invalid month,please enter number between 1-12");
+        break;
+    }
+    return 0;
+}
