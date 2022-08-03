38aaaf6725ff4649bc62e01399f699dcc0ea0853
nehatkr <nehathakur2308@gmail.com>
2022-08-03 16:43:13 +0530

    day7

--- patterns/floyds_triangle_pattern.c
+++ patterns/floyds_triangle_pattern.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    int i, j, number=1,rows;
+    int i, j, number=0,rows;
     printf("Enter number of rows :\n");
     scanf("%d", &rows);
     for ( i = 1; i <=rows ; i++)
     {
         for ( j = 1; j < i; j++)
-        {
+        {   number++;
             printf("%d ", number);
-            ++number;
         }
         printf("\n");
     }
