c1c9501145c12ef5c2429ec575d5f05c2605fd59
nehatkr <nehathakur2308@gmail.com>
2022-08-05 20:15:51 +0530

    day9

--- patterns/pascals_triangle_pattern.c
+++ patterns/pascals_triangle_pattern.c
@@ -1,26 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     int space, cols, rows, no, n = 1;
     printf("Enter the number of rows :\n");
-    scanf("%d" ,&no);
-    
+    scanf("%d", &no);
+
     for (int rows = 0; rows < no; rows++)
     {
-        for (space = 0; space < (no-rows); space++)
+        for (space = 0; space < (no - rows); space++)
         {
             printf("  ");
         }
         for (cols = 0; cols <= rows; cols++)
         {
-            if(cols==0 || rows==0)
-            n = 1;
+            if (cols == 0 || rows == 0)
+                n = 1;
             else
-            n = n * (rows - cols + 1) / (cols);
+                n = n * (rows - cols + 1) / (cols);
             printf("%4d", n);
         }
-        
-       printf("\n");
+
+        printf("\n");
     }
     return 0;
 }
