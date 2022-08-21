281dddd47e24216ac2cc94f3a94b0fe517dd1502
nehatkr <nehathakur2308@gmail.com>
2022-08-21 18:19:51 +0530

    day10

---
+++ array_and_pointer/array_matrix_formation.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+
+int number[3][3];
+int i,j;
+for ( i = 0; i < 3; i++)
+{
+    for ( j = 0; j < 3; j++)
+    {
+        printf("Enter number :");
+        scanf("%d",&number[i][j]);
+    }
+    
+}
+for ( i = 0; i< 3; i++)
+{
+    for ( j = 0; j< 3; j++)
+    {
+        printf("%d\t",number[i][j]);
+    }
+    printf("\n");
+}
+
+
+
+
+    return 0;
+}
