28c4c1f92e53ede84590b107649d10f45c0ec751
nehatkr <nehathakur2308@gmail.com>
2022-08-24 19:52:02 +0530

    day12

---
+++ array_and_pointer/array_max.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+    int arr[] = { 10,70,45,99,48,30};
+    int length = sizeof(arr)/sizeof(arr[0]);
+    int max = arr[0];
+
+     for (int i = 0; i < length; i++)
+    {
+        if(arr[i] > max)
+        max = arr[i]; 
+        
+    }
+    
+        printf("Largest in given array is %d\n",max);
+        return 0;
+}
+
+    
+    
+   
+    
+
+    
