441ba2cdd9e30823e023ac87659ce6d984c534d1
nehatkr <nehathakur2308@gmail.com>
2022-09-01 17:45:10 +0530

    day20

--- array_and_pointer/pointer_basic.c
+++ array_and_pointer/pointer_basic.c
@@ -6,15 +6,17 @@ int main(){
     int *ptra = &a;
     int *ptra2 = &a;
     int *ptra3 = NULL;
+
     
     printf("The value of a is %d\n",a);
     printf("The value of a is %d\n",*ptra);
+    printf("The value of a is %d\n",&(*ptra2));
     printf("The address of pointer to a is%p\n",&ptra);
     printf("The address of a is %p\n",&a);
     printf("The address of a is %p\n", ptra);
-    printf("the value of a is %x\n", *ptra); //%p and %x  format specifer is used for printing of address.
+    printf("the value of a is %u\n", *ptra); //%p and %x  format specifer is used for printing of address.
     printf("the value of some garbage  is %p\n", ptra2);
     printf("the value of some garbage  is %p\n", ptra3);
 
     return 0;
-}
+}           
