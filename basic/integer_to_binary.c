aeecfd9ad0acc4fa2884344dd73a1bb626fd543b
nehatkr <nehathakur2308@gmail.com>
2022-09-04 18:34:39 +0530

    day23

--- basic/integer_to_binary.c
+++ basic/integer_to_binary.c
@@ -1,18 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int num,var,remainder,binary=0,x=1;
+    int num, var, remainder, binary = 0, x = 1;
     printf("\nEnter a number");
-    scanf("%d",&num);
+    scanf("%d", &num);
 
     var = num;
-    while(num!=0){
-        remainder = num%2;
-        num = num/2;
+    while (num != 0)
+    {
+        remainder = num % 2;
+        num = num / 2;
         binary = binary + (remainder * x);
         x = x * 10;
     }
-    printf("\nBinary Equivalent of %d is %d",var,binary);
+    printf("\nBinary Equivalent of %d is %d", var, binary);
 
     return 0;
 }
