3ac8cdaa14c8cf91ac920c54e49321188baf828b
nehatkr <nehathakur2308@gmail.com>
2022-09-10 18:09:55 +0530

    day24

---
+++ array_and_pointer/swap2_no_using_pointer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int x, y, *a, *b, temp;
+    printf("Enter the value of x and y:-");
+    scanf("%d%d", &x, &y);
+
+    printf("Numbers before swapping\nx = %d\ny = %d", x, y);
+    a = &x;
+    b = &y;
+
+    temp = *b;
+    *b = *a;
+    *a = temp;
+    
+    printf("\nNumbers after swapping\nx = %d\ny = %d", x, y);
+
+    return 0;
+}
