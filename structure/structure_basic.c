0a0f02cb1bae193a4c22257a3a95bee1a24e2415
Nehatkr <nehathakur2308@gmail.com>
2022-10-07 09:29:17 +0530

    day46

---
+++ structure/structure_basic.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+struct student
+{
+    int id;
+    int marks;
+    char Fav_char;
+    char name[34];
+};
+
+int main()
+{
+    struct student Shubham, Ravi, Abhi;
+    Shubham.id = 45;
+    Ravi.id = 35;
+    Abhi.id = 65;
+    Ravi.marks = 453;
+    Shubham.marks = 334;
+    Abhi.marks = 478;
+    Ravi.Fav_char = 'R';
+    Shubham.Fav_char = 'S';
+    Abhi.Fav_char = 'A';
+
+    strcpy(Shubham.name, "suman");
+
+    printf("Shubham got %d marks \n", Shubham.marks);
+    printf("Ravi got %d marks \n", Ravi.marks);
+    printf("Abhi got %d marks \n", Abhi.marks);
+    printf("Shubham's id is: %d \n", Shubham.id);
+    printf("Ravi's id is: %d \n", Shubham.id);
+    printf("Abhi's id is: %d \n", Shubham.id);
+    printf("Shubham's fav character is: %c \n", Shubham.Fav_char);
+    printf("Ravi's fav character is: %c \n", Ravi.Fav_char);
+    printf("Abhi's fav character is: %c \n", Abhi.Fav_char);
+
+    printf("Shubham's name is %s\n", Shubham.name);
+    return 0;
+}
