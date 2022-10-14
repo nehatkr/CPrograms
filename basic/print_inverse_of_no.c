7664d5b264fca9cd20eed85e8ddcd60d2375c193
Nehatkr <nehathakur2308@gmail.com>
2022-10-14 08:49:03 +0530

    day53

--- basic/print_inverse_of_no.c
+++ basic/print_inverse_of_no.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int number, reminder, reverse = 0;
+    int number, remainder, reverse = 0;
     printf("Enter any number to reverse\n");
     scanf("%d", &number);
 
-    while (number > 0)
+    while (number > 0)                    //suppose entered no is 1234
     {
-        reminder = number % 10;
-        reverse = reverse*10 + reminder;
-        number = number/10;
+        remainder = number % 10;           //here remainder = 1234 % 10 => 4
+        reverse = reverse*10 + remainder;  //reverse = 0 *10 + 4 => 4............here we can print reaminder(4) as a first digit
+        number = number/10;                //number = 1234/10 => 123........again loop will exacute 
     }
     printf("Reverse of entered number is:%d\n",reverse);
     return 0;
