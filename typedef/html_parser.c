0840f45a1c84d13d6a2773f75c1ac609b2f25085
Nehatkr <nehathakur2308@gmail.com>
2022-10-20 11:55:28 +0530

    day59

--- typedef/html_parser.c
+++ typedef/html_parser.c
@@ -1,51 +1,49 @@
-#include<stdio.h>
-#include<string.h>
- void parser (char *string)
- {
+#include <stdio.h>
+#include <string.h>
+void parser(char *string)
+{
     int in = 0; // variable to track whether we are inside the tag.
     int index = 0;
     for (int i = 0; i < strlen(string); i++)
     {
-        if (string[i] =='<')
+        if (string[i] == '<')
         {
             in = 1;
-            continue;   
+            continue;
         }
-        else if (string[i] == '>'){
+        else if (string[i] == '>')
+        {
             in = 0;
             continue;
         }
         if (in == 0)
         {
-           string[index] = string[i];
-           index++;
+            string[index] = string[i];
+            index++;
         }
-        
     }
-    //removal of white spaces from beginning
+    // removal of white spaces from beginning
     string[index] = '\0';
     while (string[0] == ' ')
     {
-        //shift string to the left side
-        for (int  i = 0; i < strlen(string) ; i++)
+        // shift string to the left side
+        for (int i = 0; i < strlen(string); i++)
         {
-           string[i] = string[i+1];
+            string[i] = string[i + 1];
         }
-        
-
     }
-    
-    //removal of white spaces from end
-     while (string[strlen(string) - 1] == ' ')
+
+    // removal of white spaces from end
+    while (string[strlen(string) - 1] == ' ')
     {
         string[strlen(string) - 1] = '\0';
-
     }
- }
+}
 
- int main(){
+int main()
+{
     char string[] = "<h1>     This is the heading      </h1>";
     parser(string);
-    printf("The pased string is ~~%s~~",string);
+    printf("The pased string is ~~%s~~", string);
     return 0;
- }
+}
