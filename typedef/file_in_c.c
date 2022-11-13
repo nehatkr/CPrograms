25b0a849be3ed01f48144ee74914c2f215e35f4b
Nehatkr <nehathakur2308@gmail.com>
2022-11-13 18:09:24 +0530

    day82

--- typedef/file_in_c.c
+++ typedef/file_in_c.c
@@ -2,11 +2,18 @@
 int main()
 {
     FILE*ptr = NULL;
-    ptr = fopen("myfile2.txt","r");
-    char c = fgetc(ptr);
-    printf("The character  I read was %c\n",c);
-     c = fgetc(ptr);
-    printf("The character  I read was %c\n",c);
+   // ptr = fopen("myfile2.txt","r");
+   // char c = fgetc(ptr);
+   // printf("The character  I read was %c\n",c);
+    // c = fgetc(ptr);
+    //printf("The character  I read was %c\n",c);
+
+   // char str[4];
+   // fgets(str, 31, ptr);
+   // printf("The string is %s\n",str);
+   ptr = fopen("myfile2.txt","w");
+   fputc('o',ptr);
+   fputs("this is neha",ptr);
 
     fclose(ptr);
     return 0;
